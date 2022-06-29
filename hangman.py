45221519c4da3d8fb835f2b3230ac3a085678f79
Jerry Huo <jerry.yiyang.huo@gmail.com>
2022-06-29 14:50:26 -0700

    not done

--- hangman.py
+++ hangman.py
@@ -13,45 +13,79 @@ def main():
 #picks random word and prints corresponding 'empty' spaces
     word = random.choice(words)
     word.rstrip('\n')
-    spaces = ['_.'] * len(word)
-    spaces[-1] = '_'
+    spaces = ['._'] * len(word)
     for s in spaces:    
         print(s, end='')
     print("\n")
 #getting choices
     tried = []
-    choice = get_choice()
     w = []
     for char in word:
         w.append(char)
-    index = []
     del(w[-1])
+    wrong = 0
+    print(w)
+    while '._' in spaces:
+        choice = get_choice()
 #checking if choice is in word
-    check(w)
-    for i in index:
-        spaces[i] = choice
-    print(spaces)
+        c = check(w, choice)
+#if choice is in word then it replaces the "._" in spaces with the letter
+        if c is not False:
+            print(c)
+            for x in c:
+                spaces[x] = choice
+            for s in spaces:
+                print(s, end='')
+            print("\n")
+#if it's not in the word then a part of the hangman is added
+        elif c is False:
+            print("Sorry, your choice is not in the word.")
+            wrong += 1
+            print_hangman(wrong)
 
 
 def get_choice():
-    choice = input('What letter would you like to try?' )
-    if choice.isalpha is False:
+    choice = input('What letter would you like to try? ')
+    if choice.isalpha() is False:
         print('You did not choose a letter, please choose again.')
         return get_choice()
     else:
         return choice.lower()
 
 
-def check(word):
+def check(word, choice):
     check = False
-    for i in range(len(w)):
-        if choice == w[i]:
+    index = []
+    for i in range(len(word)):
+        if choice == word[i]:
             index.append(i)
-            check = True
+        check = True
+        return index
     if check == False:
-        print("Sorry, this letter is not in the word.")
+        return False
 
 
+def print_hangman(w):
+    if w == 1:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "      | ")
+        print("      | \n" + "      | \n" + "=========")
+    if w == 2:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "  |   | ")
+        print("      | \n" + "      | \n" + "=========")
+    if w == 3:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "  |   | ")
+        print(" /|   | \n" + "      | \n" + "=========")
+    if w == 4:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "  |   | ")
+        print(" /|\  | \n" + "      | \n" + "=========") 
+    if w == 5:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "  |   | ")
+        print(" /|\  | \n" + " /    | \n" + "=========")
+    if w == 6:
+        print("  +---+ \n" + "  |   | \n" + "  o   | \n" +  "  |   | ")
+        print(" /|\  | \n" + " / \  | \n" + "=========")
+        print("Oh no! The hangman was hung! You lost :(")
+        quit()
 
 
 if __name__ == '__main__':
